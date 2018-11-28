b14b64e8349c00837ec2043faebdfaf2f81e0ec3
Jan Westphal <jan.westphal306@gmail.com>
2018-11-28 14:33:25 +0100

    Start jobs and request results

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <hello/>
-    <!--
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul>
-    -->
+    <h2>Select Job:</h2>
+    <JobSelection/>
   </div>
 </template>
 
 <script>
-import Hello from "./components/Hello";
+import JobSelection from "./components/JobSelection";
 
 export default {
   name: "app",
   components: {
-    Hello
-  },
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
-  },
-  mounted() {
-    console.log("APP");
-    // this.msg = response.json;
+    JobSelection
   }
 };
 </script>
---
+++ src/components/JobSelection.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="JobSelection">
+    <div>
+      <label>Job:</label>
+      <select v-model="selectedAnalyzer">
+        <option
+          v-for="(analyzer, index) in analyzers"
+          v-bind:key="index"
+          :value="analyzer"
+        >{{analyzer}}</option>
+      </select>
+    </div>
+    <div>
+      <label>Table:</label>
+      <input v-model="table">
+    </div>
+    <div>
+      <label>Column:</label>
+      <input v-model="column">
+    </div>
+
+    <button @click="startJob">Start Job</button>
+    <h2>Pending Jobs:</h2>
+    <div v-for="(job, index) in jobs" v-bind:key="index">
+      <label>{{index + 1}}: {{job.analyzer}} {{job.jobId}}</label>
+      <button @click="checkResult(job.jobId)">Get Result</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "JobSelection",
+  data() {
+    return {
+      analyzers: [],
+      selectedAnalyzer: "",
+      jobs: [],
+      table: "food_des",
+      column: "fat_factor"
+    };
+  },
+  methods: {
+    startJob: function() {
+      axios
+        .post(`http://localhost:8080/api/jobs/${this.selectedAnalyzer}/start`, {
+          context: "jdbc",
+          table: this.table,
+          column: this.column
+        })
+        .then(response => this.jobs.push(response.data));
+    },
+    checkResult: function(jobId) {
+      axios
+        .get(`http://localhost:8080/api/jobs/${jobId}/result`)
+        .then(response => {
+          console.log(response, response.data);
+          alert(response.data.result);
+        });
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:8080/api/jobs/analyzers").then(response => {
+      this.analyzers = response.data.analyzers;
+      this.selectedAnalyzer = response.data.analyzers[0];
+    });
+  }
+};
+</script>

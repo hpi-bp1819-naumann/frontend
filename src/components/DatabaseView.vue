f7cfeb918928cdf27fd8c8b5d7dfa2b6d9004cd8
MuPiCo <katrin_klein@icloud.com>
2018-12-18 14:06:15 +0100

    hardcoded json cause route not existing yet, displays columns for selected table

--- src/components/DatabaseView.vue
+++ src/components/DatabaseView.vue
@@ -2,21 +2,32 @@
   <div class="db-view md-elevation-1">
 
     <div class="md-display-2">Database View</div>
-
       <div class="md-layout md-gutter">
         <div class="md-layout-item md-size-15">
           <md-field>
             <label>Tables</label>
             <md-select v-model="selectedTable">
               <md-option v-for="(item, index) in tables"
-                       :value="item"
+                       :value="item.table"
                        :key="index">
-                {{ item }}
+                {{ item.table }}
               </md-option>
             </md-select>
           </md-field>
         </div>
+
+        <div class="md-layout-item md-size-15">
+          <ul class="mdc-list">
+            <div v-for="(item, index) in columns[selectedTable]"
+                          :value="item"
+                          :key="index">
+                <span>{{ item }}</span>
+            </div>
+          </ul>
+        </div>
+
       </div>
+
   </div>
 </template>
 
@@ -28,17 +39,30 @@
     name: "DatabaseView",
     data() {
       return {
-        json: {},
+        json: { tables: [
+            { table: "firsttable", 
+              columns: [ "firstcolumn1", "firstcolumn2", "firstcolumn3" ] }, 
+            { table: "secondtable", 
+              columns: [ "secondcolumn1", "secondcolumn2", "secondcolumn3", "secondcolumn4" ] }, 
+            { table: "thirdtable",
+              columns: [ "thirdcolumn1" ] } 
+          ] },
         tables: [],
         selectedTable: "",
+        columns: {},
+        selectedColumn: "",
       };
     },
     mounted() {
-      axios.get("http://localhost:8080/api/db/tables").then(response => {
-        this.json = response.data;
-        this.tables = response.data.tables;
-        this.selectedTable = response.data.tables[0];
-      });
+        this.tables = this.json.tables;
+        this.json.tables.forEach(currTable =>{
+          this.columns[currTable.table] = currTable.columns;
+        })
+        this.selectedTable = this.tables[0].table;
+        this.selectedColumn = this.columns[selectedTable][0];
+      /*axios.get("http://localhost:8080/api/db/tables").then(response => {
+        
+      });*/
     }
   };
 </script>

90fb201ae40a403d792a4a5b420430f3c5486769
MuPiCo <katrin_klein@icloud.com>
2018-12-18 12:03:01 +0100

    tables in dropdown

--- src/components/DatabaseView.vue
+++ src/components/DatabaseView.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    {{json}}
+  <div class="db-view md-elevation-1">
+
+    <div class="md-display-2">Database View</div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-15">
+          <md-field>
+            <label>Tables</label>
+            <md-select v-model="selectedTable">
+              <md-option v-for="(item, index) in tables"
+                       :value="item"
+                       :key="index">
+                {{ item }}
+              </md-option>
+            </md-select>
+          </md-field>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -12,19 +28,23 @@
     name: "DatabaseView",
     data() {
       return {
-        json: {}
+        json: {},
+        tables: [],
+        selectedTable: "",
       };
     },
     mounted() {
       axios.get("http://localhost:8080/api/db/tables").then(response => {
         this.json = response.data;
+        this.tables = response.data.tables;
+        this.selectedTable = response.data.tables[0];
       });
     }
   };
 </script>
 
 <style>
-  .job-selection {
+  .db-view {
     padding: 20px;
   }
 

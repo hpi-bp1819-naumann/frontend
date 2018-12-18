d545e84729a8d6982afcf75a7b0f1dd93e2a0219
Paul Zimmermann <p.zimmermann55@googlemail.com>
2018-12-18 13:34:22 +0100

    only fields that are suited for the selected analyzer are shown, grouped the fields a bit

--- src/components/JobSelection.vue
+++ src/components/JobSelection.vue
@@ -3,46 +3,74 @@
 
     <div class="md-display-2">Select Job</div>
 
-    <div class="md-layout md-gutter">
-
-      <div class="md-layout-item md-size-15">
+    <div id="analyzer" class="md-layout md-gutter">
+      <div class="md-layout-item md-size-20">
         <md-field>
           <label>Analyzer</label>
-          <md-select v-model="selectedAnalyzer">
+          <md-select v-model="selectedAnalyzerIndex">
             <md-option v-for="(analyzer, index) in analyzers"
-                       :value="analyzer.key"
-                       :key="index">
+                       :value="index">
               {{analyzer.name}}
             </md-option>
-            <md-option value="corellation">Verdana</md-option>
           </md-select>
         </md-field>
       </div>
+    </div >
 
-
-      <div class="md-layout-item md-size-15">
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.context">
         <md-field>
           <label>Context</label>
           <md-select v-model="selectedContext">
-            <md-option v-for="(context, index) in contexts" v-bind:key="index" :value="context">{{context}}</md-option>
+            <md-option v-for="(context, index) in contexts" :key="index" :value="context">{{context}}</md-option>
           </md-select>
         </md-field>
       </div>
 
-      <div class="md-layout-item md-size-15">
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.table">
         <md-field>
           <label>Tablename</label>
           <md-input v-model="table" spellcheck="false"></md-input>
         </md-field>
       </div>
 
-      <div class="md-layout-item md-size-15">
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.column">
         <md-field>
           <label>Columnname</label>
           <md-input v-model="column" spellcheck="false"></md-input>
         </md-field>
       </div>
+    </div>
 
+    <div class="md-layout md-gutter">
+
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.where">
+        <md-field>
+          <label>WHERE</label>
+          <!--<md-input v-model="column" spellcheck="false"></md-input>-->
+        </md-field>
+      </div>
+
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.instance">
+        <md-field>
+          <label>Instance</label>
+          <!--<md-input v-model="column" spellcheck="false"></md-input>-->
+        </md-field>
+      </div>
+
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.predicate">
+        <md-field>
+          <label>Predicate</label>
+          <!--<md-input v-model="column" spellcheck="false"></md-input>-->
+        </md-field>
+      </div>
+
+      <div class="md-layout-item md-size-20" v-if="selectedAnalyzer.pattern">
+        <md-field>
+          <label>Pattern</label>
+          <!--<md-input v-model="column" spellcheck="false"></md-input>-->
+        </md-field>
+      </div>
     </div>
 
     <md-button class="md-dense md-raised md-primary" @click="startJob">Start Job</md-button>
@@ -59,20 +87,13 @@
               Get Result
             </md-button>
 
-            Result: <span  v-if="results[index]"> {{results[index]}} </span>
+            Result:  <span v-if="results[index]">  {{results[index]}} </span>
           </md-list-item>
         </md-list>
 
       </div>
     </div>
 
-
-    <!--<div v-for="(job, index) in jobs" v-bind:key="index">-->
-      <!--<label>{{index + 1}}: {{job.analyzer}} {{job.jobId}}</label>-->
-      <!--<md-button @click="checkResult(job.jobId)" class="md-dense md-raised">-->
-        <!--Get Result-->
-      <!--</md-button>-->
-    <!--</div>-->
   </div>
 </template>
 
@@ -87,26 +108,38 @@
         analyzers: [],
         contexts: ["jdbc", "spark"],
         jobs: [],
-        selectedAnalyzer: "",
+        selectedAnalyzer: {},
+        selectedAnalyzerIndex: null,
         selectedContext: "jdbc",
         table: "food_des",
         column: "fat_factor",
         results: [],
       };
     },
+
     watch: {
-      results: function(){},
+      selectedAnalyzerIndex: function () {
+        this.selectedAnalyzer = {
+          name: this.analyzers[this.selectedAnalyzerIndex].name,
+          key: this.analyzers[this.selectedAnalyzerIndex].key,
+        };
+        for (let option of this.analyzers[this.selectedAnalyzerIndex].parameters) {
+          this.selectedAnalyzer[option.name] = true;
+        }
+        console.log(this.selectedAnalyzer);
+      }
     },
+
     methods: {
       startJob: function () {
 
-        console.log("selectedAnalyzer: ", this.selectedAnalyzer.toLowerCase());
+        console.log("selectedAnalyzer: ", this.selectedAnalyzer.name);
         console.log("context: ", this.selectedContext);
         console.log("table: ", this.table);
         console.log("column: ", this.column);
 
         axios
-          .post(`http://localhost:8080/api/jobs/${this.selectedAnalyzer}/start`, {
+          .post(`http://localhost:8080/api/jobs/${this.selectedAnalyzer.key}/start`, {
             context: this.selectedContext,
             table: this.table,
             column: this.column
@@ -117,19 +150,15 @@
         axios
           .get(`http://localhost:8080/api/jobs/${jobId}/result`)
           .then(response => {
-            // console.log(response, response.data);
-            let resultsCopy = JSON.parse(JSON.stringify(this.results));
-            resultsCopy[jobIndex] = response.data.result;
-            this.results = resultsCopy;
-            console.log(this.results);
-            // alert(response.data.result);
+            this.results[jobIndex] = response.data.result;
+            this.results = JSON.parse(JSON.stringify(this.results));
           });
       }
     },
     mounted() {
       axios.get("http://localhost:8080/api/jobs/analyzers").then(response => {
         this.analyzers = response.data.analyzers;
-        this.selectedAnalyzer = response.data.analyzers[0];
+        this.selectedAnalyzer.name = response.data.analyzers[0];
       });
     }
   };
@@ -140,9 +169,6 @@
     padding: 20px;
   }
 
-  .md-field {
-    max-width: 300px;
-  }
 
   #pending {
     margin-top: 20px;

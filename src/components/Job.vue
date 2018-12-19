178382bf2b4c101cc3b77f3c7984ba121ff10a8d
Jan Westphal <jan.westphal306@gmail.com>
2018-12-19 13:05:26 +0100

    Extend job details

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <h1>Job Details</h1>
-    {{this.$route.params.jobId}}
-    {{this.job.result}}
+    <md-list>
+      <md-list-item>{{this.job.id}}</md-list-item>
+      <md-list-item>Status: {{this.job.status}}</md-list-item>
+      <md-list-item>Started: {{this.job.startingTime}}</md-list-item>
+      <md-list-item v-if="job.status === 'completed'">Finished: {{this.job.finishingTime}}</md-list-item>
+      <md-list-item
+        v-if="job.status === 'completed'"
+      >Duration: {{msToTime(this.job.finishingTime - this.job.startingTime)}}</md-list-item>
+      <div v-if="job.status === 'completed'">
+        <!-- hacky way to distingush between Histogram and others -->
+        <md-list-item v-if="job.result.value.values">Histogram</md-list-item>
+        <md-list-item v-else>Result: {{this.job.result.value}}</md-list-item>
+      </div>
+    </md-list>
   </div>
 </template>
 
@@ -13,7 +25,13 @@ export default {
   name: "Job",
   data() {
     return {
-      job: {}
+      job: {
+        id: "",
+        status: "",
+        result: {},
+        startingTime: "",
+        finishingTime: ""
+      }
     };
   },
 
@@ -28,16 +46,39 @@ export default {
   methods: {
     fetchData() {
       axios
-        .get(
-          `http://localhost:8080/api/jobs/${this.$route.params.jobId}/result`
-        )
+        .get(`http://localhost:8080/api/jobs/${this.$route.params.jobId}`)
         .then(resp => {
-          this.job = resp.data;
+          var job = resp.data.job;
+          job.startingTime = new Date(job.startingTime);
+          if (job.status === "completed") {
+            job.finishingTime = new Date(job.finishingTime);
+          }
+          this.job = job;
         })
         .catch(err => {
           console.log(this.$route);
           this.$router.push("/jobs");
         });
+    },
+    msToTime(duration) {
+      var result = "";
+      var milliseconds = parseInt(duration % 1000),
+        seconds = parseInt((duration / 1000) % 60),
+        minutes = parseInt((duration / (1000 * 60)) % 60),
+        hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+      if (hours > 0) {
+        result += hours + "h ";
+      }
+      if (minutes > 0 || hours > 0) {
+        result += minutes + "min ";
+      }
+      if (seconds > 0 || minutes > 0 || hours > 0) {
+        result += seconds + "sec ";
+      }
+      result += milliseconds + "msec";
+
+      return result;
     }
   }
 };

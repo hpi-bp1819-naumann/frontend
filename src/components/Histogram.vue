3d758ba143e365add374841e7b20f332292f71b0
Jan Westphal <jan.westphal306@gmail.com>
2019-01-09 11:28:00 +0100

    Add sorting option for histogram

--- src/components/Histogram.vue
+++ src/components/Histogram.vue
@@ -7,6 +7,30 @@
           <md-input v-model="minimumBucketSize" type="number" min="1"></md-input>
         </md-field>
       </div>
+
+      <div class="md-layout md-gutter">
+        <div class="md-layout-item md-size-20">
+          <md-field>
+            <label>Sort by Bucket Name</label>
+            <md-select v-model="sortByBucketName">
+              <md-option value="none" key="none">None</md-option>
+              <md-option value="asc" key="asc">Ascending</md-option>
+              <md-option value="desc" key="desc">Descending</md-option>
+            </md-select>
+          </md-field>
+        </div>
+
+        <div class="md-layout-item md-size-20">
+          <md-field>
+            <label>Sort by Bucket Size</label>
+            <md-select v-model="sortByBucketSize">
+              <md-option value="none" key="none">None</md-option>
+              <md-option value="asc" key="asc">Ascending</md-option>
+              <md-option value="desc" key="desc">Descending</md-option>
+            </md-select>
+          </md-field>
+        </div>
+      </div>
     </div>
     <div :ref="id" style="width:90%;height:400px;"></div>
   </div>
@@ -18,7 +42,9 @@ export default {
   data() {
     return {
       id: "histogramPlot",
-      minimumBucketSize: 1
+      minimumBucketSize: 1,
+      sortByBucketSize: "none",
+      sortByBucketName: "none"
     };
   },
   mounted() {
@@ -27,16 +53,20 @@ export default {
   watch: {
     minimumBucketSize: function() {
       this.plot();
+    },
+    sortByBucketSize: function() {
+      this.plot();
+    },
+    sortByBucketName: function() {
+      this.plot();
     }
   },
   methods: {
     plot: function() {
       const frequencies = this.frequencies;
-      const filteredKeys = Object.keys(frequencies).filter(
-        key => frequencies[key].absolute >= this.minimumBucketSize
-      );
-      const absolute = filteredKeys.map(key => frequencies[key].absolute);
-      const keys = filteredKeys;
+      const keys = this.prepareKeys();
+      const absolute = keys.map(key => frequencies[key].absolute);
+
       const trace = {
         x: keys,
         y: absolute,
@@ -52,6 +82,29 @@ export default {
       };
       const data = [trace];
       Plotly.newPlot(this.$refs[this.id], data, layout);
+    },
+    prepareKeys: function() {
+      const frequencies = this.frequencies;
+      const keys = Object.keys(frequencies).filter(
+        key => frequencies[key].absolute >= this.minimumBucketSize
+      );
+      if (this.sortByBucketName === "asc") {
+        keys.sort();
+      } else if (this.sortByBucketName === "desc") {
+        keys.sort();
+        keys.reverse();
+      }
+
+      if (this.sortByBucketSize === "asc") {
+        keys.sort(function(a, b) {
+          return frequencies[a].absolute - frequencies[b].absolute;
+        });
+      } else if (this.sortByBucketSize === "desc") {
+        keys.sort(function(a, b) {
+          return frequencies[b].absolute - frequencies[a].absolute;
+        });
+      }
+      return keys;
     }
   }
 };

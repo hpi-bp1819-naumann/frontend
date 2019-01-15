697fd7ba63a81dfdd77b80c0175fcb95b4f4d2c2
Jan Westphal <jan.westphal306@gmail.com>
2019-01-15 14:30:18 +0100

    Remove vue-js-modal

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import router from "./router";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
-import Vuelidate from 'vuelidate'
-import VModal from 'vue-js-modal'
+import Vuelidate from "vuelidate";
 
 Vue.use(VueMaterial);
 Vue.use(Vuelidate);
-Vue.use(VModal, { dynamic: true });
 
 new Vue({
   el: "#app",

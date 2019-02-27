561d671e691b635784ddd1eea8903d8d7d9b762d
MuPiCo <katrin_klein@icloud.com>
2019-02-27 09:51:05 +0100

    fix bug after merge

--- src/components/JobSelection.vue
+++ src/components/JobSelection.vue
@@ -192,7 +192,6 @@ export default {
   name: "JobSelection",
   data() {
     return {
-      test: "",
       areFieldsValid: [true, true, []], // 0 tableName, 1 columnName, 2+ [] Array of Values for field
       analyzers: [],
       contexts: ["jdbc", "spark"],
@@ -240,7 +239,7 @@ export default {
     startSingleJob: function(jobAnalyzer) {
       let requestObject = {
         context: this.context,
-        table: this.table,
+        table: this.selectedTable,
         where: jobAnalyzer.where ? jobAnalyzer.where : null,
         instance: jobAnalyzer.instance,
         predicate: jobAnalyzer.predicate,
